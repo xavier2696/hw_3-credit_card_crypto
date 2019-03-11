fcc74bfab1d05f626cb4f4e35cc63b879a9552d7
Xavier <xavier2696@gmail.com>
2019-03-11 23:33:22 +0800

    Part A: luhn_validator.rb and credit_card.rb implementation

--- credit_card.rb
+++ credit_card.rb
@@ -1,20 +1,31 @@
+# frozen_string_literal: true
+
 require_relative './luhn_validator.rb'
 require 'json'
 
+# class CreditCard
 class CreditCard
   # TODO: mixin the LuhnValidator using an 'include' statement
-
+  include LuhnValidator
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
     # TODO: initialize the instance variables listed above
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
   end
 
   # returns json string
   def to_json
     {
-      # TODO: setup the hash with all instance vairables to serialize into json
+      # TODO: setup the hash with all instance variables to serialize into json
+      number: @number,
+      expiration_date: @expiration_date,
+      owner: @owner,
+      credit_network: @credit_network
     }.to_json
   end
 
--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# module LuhnValidator
 module LuhnValidator
   # Validates credit card number using Luhn Algorithm
   # arguments: none
@@ -5,7 +8,19 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
+    sum_digits = nums_a.reverse.map.with_index { |v, i| return_value(v, i) }
+    sum = sum_digits.reduce { |n1, n2| n1 + n2 }
+    (sum % 10).zero?
+  end
 
-    # TODO: use the integers in nums_a to validate its last check digit
+  def return_value(value, index)
+    return_value = if index.even?
+                     value
+                   elsif value * 2 <= 9
+                     value * 2
+                   else
+                     value * 2 - 9
+                   end
+    return_value
   end
 end
