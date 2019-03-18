93830e9fa999493e6ebbd4e8f26f26547b34aabd
Xavier <xavier2696@gmail.com>
2019-03-19 00:50:25 +0800

    made code idiomatic for part:b

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# DoubleTranspositionCipher module
 module DoubleTranspositionCipher
   def self.encrypt(document, key)
     # TODO: FILL THIS IN!
@@ -7,34 +10,48 @@ module DoubleTranspositionCipher
     # 3. sort rows in predictibly random way using key as seed
     # 4. sort columns of each row in predictibly random way
     # 5. return joined cyphertext
-    n,m = self.get_matrix_size(document.to_s)
+    n, m = get_matrix_size(document.to_s)
     document = get_padded_string(document.to_s, n * m)
-    matrix = document.chars.each_slice(n).map { |e| e}
+    matrix = document.chars.each_slice(n).map { |e| e }
     r_array = (0...n).to_a.shuffle!(random: Random.new(key))
-    matrix.map { |row| row.map.with_index { |v, i|  row[r_array[i]]}.reduce{ |n1,n2| "#{n1}#{n2}" } }.reduce{ |n1,n2| "#{n1}#{n2}" }
+    get_encrypted_string(matrix, r_array)
   end
 
   def self.decrypt(document, key)
     # TODO: FILL THIS IN!
-    n,m = self.get_matrix_size(document.to_s)
-    matrix = document.to_s.chars.each_slice(n).map { |e| e}
+    n = get_matrix_size(document.to_s)
+    matrix = document.to_s.chars.each_slice(n).map { |e| e }
     r_array = (0...n).to_a.shuffle!(random: Random.new(key))
-    matrix.map { |row| row.map.with_index { |v, i|  row[r_array.index(i)]}.reduce{ |n1,n2| "#{n1}#{n2}" } }.reduce{ |n1,n2| "#{n1}#{n2}" }.strip
+    get_decrypted_string(matrix, r_array)
   end
 
   def self.get_matrix_size(string)
     size = Integer.sqrt(string.length)
-    if string.length == size * size
-      return size, size
-    elsif string.length < (size + 1) * size
-      return size + 1, size
-    else
-      return size + 1, size + 1
-    end
+    return size, size unless string.length != size * size
+    return size + 1, size unless string.length >= (size + 1) * size
+
+    [size + 1, size + 1]
   end
 
   def self.get_padded_string(string, size)
     return string if string.length == size
-    string + ([" "] * (size - string.length)).reduce{ |n1,n2| "#{n1}#{n2}" }
+
+    string + ([' '] * (size - string.length)).reduce { |n1, n2| "#{n1}#{n2}" }
+  end
+
+  def self.get_encrypted_string(matrix, r_array)
+    encrypted = matrix.map do |row|
+      row.map.with_index { |_v, i| row[r_array[i]] }
+         .reduce { |n1, n2| "#{n1}#{n2}" }
+    end
+    encrypted.reduce { |n1, n2| "#{n1}#{n2}" }
+  end
+
+  def self.get_decrypted_string(matrix, r_array)
+    decrypted = matrix.map do |row|
+      row.map.with_index { |_v, i| row[r_array.index(i)] }
+         .reduce { |n1, n2| "#{n1}#{n2}" }
+    end
+    decrypted.reduce { |n1, n2| "#{n1}#{n2}" }.strip
   end
 end
