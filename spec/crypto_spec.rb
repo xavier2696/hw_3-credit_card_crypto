2c95785ca2050db1b5227044e3214281dd40ec98
Xavier <xavier2696@gmail.com>
2019-03-21 15:58:56 +0800

    made crypto_spec.rb code idiomatic

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -7,17 +7,20 @@ require 'minitest/autorun'
 
 describe 'Test card info encryption' do
   cc = CreditCard.new('4916603231464963', 'Mar-30-2020',
-                         'Soumya Ray', 'Visa')
+                      'Soumya Ray', 'Visa')
   key = 3
   encoded_values = [SubstitutionCipher::Caesar.encrypt(cc, key),
-      SubstitutionCipher::Permutation.encrypt(cc, key),
-      DoubleTranspositionCipher.encrypt(cc, key)]
+                    SubstitutionCipher::Permutation.encrypt(cc, key),
+                    DoubleTranspositionCipher.encrypt(cc, key)]
   decoded_values = [SubstitutionCipher::Caesar.decrypt(encoded_values[0], key),
-      SubstitutionCipher::Permutation.decrypt(encoded_values[1], key),
-      DoubleTranspositionCipher.decrypt(encoded_values[2], key)]
-  encoded_decoded_array = [["Caesar", encoded_values[0], decoded_values[0]],
-    ["Permutation", encoded_values[1], decoded_values[1]],
-    ["Double Transposition", encoded_values[2], decoded_values[2]]]
+                    SubstitutionCipher::Permutation.decrypt(encoded_values[1],
+                                                            key),
+                    DoubleTranspositionCipher.decrypt(encoded_values[2], key)]
+  encoded_decoded_array = [['Caesar', encoded_values[0], decoded_values[0]],
+                           ['Permutation', encoded_values[1],
+                            decoded_values[1]],
+                           ['Double Transposition', encoded_values[2],
+                            decoded_values[2]]]
 
   encoded_decoded_array.each do |c_name, enc, dec|
     describe "Using #{c_name} cipher" do
